9ac71a24d2a70100a54c1ca91daf552c7d827945
Polina Tkachenko <Polina.granger.14.02@gmail.com>
2026-03-05 02:23:00 +0200

    Weather prediction for next 24 hours

---
+++ data_receiver/getweather_24hours.py
@@ -0,0 +1,48 @@
+import datetime as dt
+import json
+import requests
+import os
+
+VCRO_KEY = ""
+
+def save_weather_data(data: json, location: str):
+    os.makedirs("data/weather_forecast", exist_ok=True)
+    now_str = dt.datetime.now().strftime("%Y-%m-%d_%H-%M")
+    filename = f"data/weather_forecast/forecast_{location}_{now_str}.json"
+    with open(filename, 'w') as f:
+        json.dump(data, f, indent=4)
+    print(f"Data saved to {filename}")
+
+def get_weather(location: str):
+    url_base_url = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline"
+
+    today = dt.datetime.now().date()
+    tomorrow = today + dt.timedelta(days=1)
+
+    url = f"{url_base_url}/{location}/{today}/{tomorrow}?unitGroup=metric&include=hours&key={VCRO_KEY}"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        data = response.json()
+
+        now = dt.datetime.now()
+        next_24_hours = []
+
+        for day in data.get("days", []):
+            for hour in day.get("hours", []):
+                hour_time = dt.datetime.strptime(f"{day['datetime']} {hour['datetime']}","%Y-%m-%d %H:%M:%S")
+                if hour_time >= now:
+                    next_24_hours.append(hour)
+
+        next_24_hours = next_24_hours[:24]
+
+        save_weather_data(next_24_hours, location)
+        return next_24_hours
+
+    else:
+        raise Exception(f"{response.status_code}: {response.text}")
+
+if __name__ == "__main__":
+    location = "Odesa, Ukraine"
+    result = get_weather(location)
+    print(json.dumps(result, indent=4))
