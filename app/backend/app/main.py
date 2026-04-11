30026b43111fb9e15f6d7ecb1d17ab2a3f9184ce
Marykasss <m.vakorina@ukma.edu.ua>
2026-04-11 18:50:13 +0300

    Update main.py

--- app/backend/app/main.py
+++ app/backend/app/main.py
@@ -11,8 +11,8 @@ app.add_middleware(
         "http://localhost:3000",
         "http://127.0.0.1:3000",
         "http://localhost:5173",
-
         "http://127.0.0.1:5173",
+        "http://192.168.0.101:5173",
     ],
     allow_credentials=True,
     allow_methods=["*"],
